e92db9e2cadcbaf2fb01a9bac2e93e0138cc76c3
Alex Trottier <postables@rtradetechnologies.com>
2020-01-29 00:25:20 -0800

    more work

--- bridge/bridge.go
+++ bridge/bridge.go
@@ -1,28 +1,47 @@
 package main
 
 import (
-  "time"
+	"context"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 
-  "gobot.io/x/gobot"
-  "gobot.io/x/gobot/drivers/gpio"
-  "gobot.io/x/gobot/platforms/firmata"
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/platforms/firmata"
 )
 
 func main() {
-  firmataAdaptor := firmata.NewAdaptor("/dev/ttyACM0")
-  led := gpio.NewLedDriver(firmataAdaptor, "13")
+	ctx, cancel := context.WithCancel(context.Background())
+	firmataAdaptor := firmata.NewAdaptor("/dev/ttyACM0")
 
-  work := func() {
-    gobot.Every(1*time.Second, func() {
-      led.Toggle()
-    })
-  }
+	robot := gobot.NewRobot("bot",
+		[]gobot.Connection{firmataAdaptor},
+	)
+	doneChan := make(chan bool, 1)
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go handleExit(ctx, cancel, wg, doneChan)
+	robot.Start()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		<-doneChan
+		robot.Stop()
+	}()
+	wg.Wait()
+}
 
-  robot := gobot.NewRobot("bot",
-    []gobot.Connection{firmataAdaptor},
-    []gobot.Device{led},
-    work,
-  )
-
-  robot.Start()
+func handleExit(ctx context.Context, cancelFunc context.CancelFunc, wg *sync.WaitGroup, doneChan chan bool) {
+	defer wg.Done()
+	// make a channel to catch os signals on
+	quitCh := make(chan os.Signal, 1)
+	// register the types of os signals to trap
+	signal.Notify(quitCh, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+	// wait until we receive an exit signal
+	<-quitCh
+	// cancel the context which will trigger shutdown of service components
+	cancelFunc()
+	// notify that we are finished handling all exit procedures
+	doneChan <- true
 }
