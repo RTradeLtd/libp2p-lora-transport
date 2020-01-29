90c7d3098da7483c41fffe368295ed47cf7022fd
Alex Trottier <postables@rtradetechnologies.com>
2020-01-29 01:37:42 -0800

    arduino: add "fast" bridge

---
+++ arduino/lora_bridge_fast.ino
@@ -0,0 +1,53 @@
+#include <LoRa.h>
+/* LibP2P LoRa Transport Arduino Bridge
+ * Enables a LibP2P node to send messages over LoRa and possibly LoRaWAN.
+ * Data coming in through the serial interface leaves through the LoRa radio
+ * Data coming in through the LoRa radio exits the serial interface
+ * This is the "fast" version and operates on bytes instead of strings
+ */
+
+#define BAND 915E6
+#define SYNC 0x99
+#define SF 7
+#define TXPOWER 20
+#define SIGBW 250E3
+#define DATASENT 0xd34db33f
+/*
+API documentation: https://github.com/sandeepmistry/arduino-LoRa/blob/master/API.md
+*/
+
+void setup() {
+    Serial.begin(115200);
+    while (!Serial);
+    LoRa.begin(BAND);
+    LoRa.setSyncWord(SYNC);
+    LoRa.setSpreadingFactor(SF);
+    LoRa.setTxPower(TXPOWER);
+    //LoRa.enableCrc();
+  //  LoRa.setSignalBandwidth(SIGBW);
+    Serial.println("ready");
+    Serial.flush();
+}
+
+
+void loop() {
+  // see if we have any data on the LoRa radio
+  // if we do, send it down the serial interface
+  if (LoRa.parsePacket()) {
+      char buffer[255];
+      int num = LoRa.readBytes(buffer, 255);
+      Serial.write(buffer, num);
+  }
+  // see if we have any data on the serial interface
+  // if we do send it down hte LoRa radio
+  if (Serial.available()) {
+    if (LoRa.beginPacket()) { // beginPacket returns 1 if ready to proceed, so wait until radio is ready
+        char buffer[255]; // 255 byte buffer
+        int num = Serial.readBytesUntil('\n', buffer, 255);
+        LoRa.write(buffer, num);
+        LoRa.endPacket(true); // async mode
+        Serial.println(DATASENT);
+        Serial.flush();
+    } 
+  }
+}
