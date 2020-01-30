c47893d1979d054edcf96a0a4bc2590afa96ccc4
Alex Trottier <postables@rtradetechnologies.com>
2020-01-29 16:45:15 -0800

    arduino: enable dbeug mode

--- arduino/lora_bridge_fast.ino
+++ arduino/lora_bridge_fast.ino
@@ -1,10 +1,10 @@
 #include <LoRa.h>
 /* LibP2P LoRa Transport Arduino Bridge
- * Enables a LibP2P node to send messages over LoRa and possibly LoRaWAN.
- * Data coming in through the serial interface leaves through the LoRa radio
- * Data coming in through the LoRa radio exits the serial interface
- * This is the "fast" version and operates on bytes instead of strings
- */
+   Enables a LibP2P node to send messages over LoRa and possibly LoRaWAN.
+   Data coming in through the serial interface leaves through the LoRa radio
+   Data coming in through the LoRa radio exits the serial interface
+   This is the "fast" version and operates on bytes instead of strings
+*/
 
 #define BAND 915E6
 #define SYNC 0x99
@@ -12,40 +12,52 @@
 #define TXPOWER 20
 #define SIGBW 250E3
 #define DATASENT 0xd34db33f
+
 /*
-API documentation: https://github.com/sandeepmistry/arduino-LoRa/blob/master/API.md
-Issues with timing: https://github.com/sandeepmistry/arduino-LoRa/issues/321
-Other example:
-  * https://github.com/IoTThinks/EasyLoRaGateway_v2.1/blob/master/EasyLoRaGateway/09_lora.ino
-  * Useful doc https://www.arduino.cc/en/Tutorial/SerialCallResponse
+  API documentation: https://github.com/sandeepmistry/arduino-LoRa/blob/master/API.md
+  Issues with timing: https://github.com/sandeepmistry/arduino-LoRa/issues/321
+  Other example:
+    https://github.com/IoTThinks/EasyLoRaGateway_v2.1/blob/master/EasyLoRaGateway/09_lora.ino
+    Useful doc https://www.arduino.cc/en/Tutorial/SerialCallResponse
 */
 
 void setup() {
-    Serial.begin(115200);
-    while (!Serial);
-    LoRa.begin(BAND);
-    LoRa.setSyncWord(SYNC);
-    LoRa.setSpreadingFactor(SF);
-    LoRa.setTxPower(TXPOWER);
-    // register callbacks
-    LoRa.onReceive(onReceive);
-    LoRa.onTxDone(onTxDone);
-    LoRa.enableCrc();
-    //  LoRa.setSignalBandwidth(SIGBW);
-    LoRa.receive(); // set receive mode, allows using callback
-    Serial.println("ready");
-    Serial.flush();
+  Serial.begin(115200);
+  while (!Serial);
+  LoRa.begin(BAND);
+  LoRa.setSyncWord(SYNC);
+  LoRa.setSpreadingFactor(SF);
+  LoRa.setTxPower(TXPOWER);
+  // register callbacks
+  LoRa.onReceive(onReceive);
+  LoRa.onTxDone(onTxDone);
+  LoRa.enableCrc();
+  //  LoRa.setSignalBandwidth(SIGBW);
+  LoRa.receive(); // set receive mode, allows using callback
+  Serial.println("ready");
+  Serial.flush();
 }
 
 // callback function whenever we receive a LoRa packet
 void onReceive(int packetSize) {
   if (packetSize) {
-      char buffer[255];
-      int num = LoRa.readBytes(buffer, 255);
-      Serial.write(buffer, num);
-      Serial.println();
-      Serial.flush();
+    char buffer[255];
+    int num = LoRa.readBytes(buffer, 255);
+    Serial.write(buffer, num);
+    Serial.println();
+    Serial.flush();
   }
+}
+
+// debug callback handler, when set intercepts
+// the next packet, printing debug info to serial
+// followed by going back to non-debug onreceive callback
+void onReceiveDebug(int packetSize) {
+  if (packetSize) {
+    Serial.println("rssi: " + String(LoRa.packetRssi()) + " snr: " + String(LoRa.packetSnr()) + " errFreq: " + String(LoRa.packetFrequencyError()) + " \n");
+    Serial.flush();    
+  }
+  LoRa.onReceive(onReceive);
 }
 
 // callback function whenever we finished sending a LoRa packet
@@ -59,11 +71,17 @@ void loop() {
   // see if we have any data on the serial interface
   // if we do send it down hte LoRa radio
   if (Serial.available()) {
-    if (LoRa.beginPacket()) { // beginPacket returns 1 if ready to proceed, so wait until radio is ready
-        char buffer[255]; // 255 byte buffer
-        int num = Serial.readBytesUntil('\n', buffer, 255);
+    char buffer[255]; // 255 byte buffer
+    int num = Serial.readBytesUntil('\n', buffer, 255);
+    if (num == 1) {
+      if (buffer[0] == '2') { // debug mode
+        LoRa.onReceive(onReceiveDebug);
+      }
+    } else if (num > 1) {
+      if (LoRa.beginPacket()) { // beginPacket returns 1 if ready to proceed, so wait until radio is ready
         LoRa.write(buffer, num);
         LoRa.endPacket(true); // async mode
-    } 
+      }
+    }
   }
 }
