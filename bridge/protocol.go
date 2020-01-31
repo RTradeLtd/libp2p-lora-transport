52f97d0b23507b1868b6725b6698248fae251101
Alex Trottier <postables@rtradetechnologies.com>
2020-01-30 21:21:01 -0800

    bridge: refactor to be non-blocking

--- bridge/protocol.go
+++ bridge/protocol.go
@@ -21,8 +21,10 @@ var ProtocolID = protocol.ID("/liblora-bridge/0.0.1")
 type Bridge struct {
 	serial          *term.Term
 	ctx             context.Context
-	mx              sync.Mutex
+	wg              *sync.WaitGroup
 	authorizedPeers map[peer.ID]bool
+	readChan        chan []byte
+	writeChan       chan []byte
 	logger          *zap.Logger
 }
 
@@ -34,49 +36,51 @@ type Opts struct {
 }
 
 // NewBridge returns an initialized bridge, suitable for use a LibP2P protocol
-func NewBridge(ctx context.Context, logger *zap.Logger, opt Opts) (*Bridge, error) {
+func NewBridge(ctx context.Context, wg *sync.WaitGroup, logger *zap.Logger, opt Opts) (*Bridge, error) {
 	trm, err := term.Open(opt.SerialDevice, term.Speed(opt.Baud))
 	if err != nil {
 		return nil, err
 	}
-	return &Bridge{
+	bridge := &Bridge{
 		serial:          trm,
 		ctx:             ctx,
 		authorizedPeers: opt.AuthorizedPeers,
 		logger:          logger.Named("lora.bridge"),
-	}, nil
+		readChan:        make(chan []byte),
+		writeChan:       make(chan []byte),
+		wg:              wg,
+	}
+	go bridge.serialDumper(wg)
+	return bridge, nil
 }
 
-// StreamHandler is used to open a bi-directional stream.
-// Only one stream may be opened at a time
-func (b *Bridge) StreamHandler(stream network.Stream) {
-	defer stream.Reset()
-	if b.authorizedPeers != nil && !b.authorizedPeers[stream.Conn().RemotePeer()] {
-		_, err := stream.Write([]byte("unauthorized"))
-		if err != nil {
-			b.logger.Error("failed to write response back")
-		}
-	}
-	b.mx.Lock()
-	defer b.mx.Unlock()
-	wg := &sync.WaitGroup{}
-	wg.Add(2)
+// serialDumper allows any number of libp2p streams to write
+// into the LoRa bridge, or read from it. Reads are sent to any
+// active streams
+func (b *Bridge) serialDumper(wg *sync.WaitGroup) {
+	wg.Add(1)
 	go func() {
 		defer wg.Done()
 		for {
 			select {
 			case <-b.ctx.Done():
 				return
-			default:
-				size, err := b.serial.Available()
+			case data := <-b.writeChan:
+				_, err := b.serial.Write(data)
 				if err != nil && err != io.EOF {
-					b.logger.Error("error getting available serial data", zap.Error(err))
+					b.logger.Error("failed to write into serial interface", zap.Error(err))
 					return
 				}
-				if size == 0 {
+			default:
+				avail, err := b.serial.Available()
+				if err != nil && err != io.EOF {
+					b.logger.Error("serial dumper failed to read available bytes", zap.Error(err))
+					return
+				}
+				if avail == 0 {
 					continue
 				}
-				data := make([]byte, size)
+				data := make([]byte, avail)
 				s, err := b.serial.Read(data)
 				if err != nil && err != io.EOF {
 					b.logger.Error("error reading serial data", zap.Error(err))
@@ -86,38 +90,42 @@ func (b *Bridge) StreamHandler(stream network.Stream) {
 				if data[0] != '^' || data[len(data)-1] != '^' {
 					continue
 				}
-				_, err = stream.Write(data[:s])
+				b.readChan <- data[:s]
+			}
+		}
+	}()
+}
+
+// StreamHandler is used to open a bi-directional stream.
+func (b *Bridge) StreamHandler(stream network.Stream) {
+	defer stream.Reset()
+	if b.authorizedPeers != nil && !b.authorizedPeers[stream.Conn().RemotePeer()] {
+		_, err := stream.Write([]byte("unauthorized"))
+		if err != nil {
+			b.logger.Error("failed to write response back")
+		}
+	}
+	reader := bufio.NewReader(stream)
+	for {
+		select {
+		case <-b.ctx.Done():
+			return
+		case data := <-b.readChan:
+			_, err := stream.Write(data)
+			if err != nil {
+				b.logger.Error("failed to write into stream")
+				return
+			}
+		default:
+			if reader.Size() > 0 {
+				data := make([]byte, reader.Size())
+				_, err := reader.Read(data)
 				if err != nil {
-					b.logger.Error("failed to write serial data", zap.Error(err))
+					b.logger.Error("failed to read data from stream buffer", zap.Error(err))
 					return
 				}
+				b.writeChan <- data
 			}
 		}
-	}()
-	go func() {
-		defer wg.Done()
-		reader := bufio.NewReader(stream)
-		for {
-			select {
-			case <-b.ctx.Done():
-				return
-			default:
-				if reader.Buffered() > 0 {
-					data := make([]byte, reader.Buffered())
-					_, err := reader.Read(data)
-					if err != nil {
-						b.logger.Error("failed to read data", zap.Error(err))
-						return
-					}
-					_, err = b.serial.Write(append(data, '\n'))
-					if err != nil {
-						b.logger.Error("failed to write into serial interface", zap.Error(err))
-						return
-					}
-				}
-			}
-		}
-	}()
-	wg.Wait()
-
+	}
 }
